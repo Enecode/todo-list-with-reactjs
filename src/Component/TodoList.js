5b94ef2829b084cfdeaf155f7c2ca1e6590a0401
Isah Jacob <isahjacob0@gmail.com>
2022-12-28 03:12:08 +0100

    the componenent handle the TodoList and the TodoItem components and the TodoList component is handling the TodoItem component

--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,12 +1,11 @@
  import React from "react";
 import Todo from "./Todo"
 
- function TodoList({todos, setTodos, setTodo}){
-    console.log(todos)
+ function TodoList({todos, setTodos, filteredTodos}){
     return(
         <div className="todo-container" >
             <ul className="todo-list">
-               {todos.map(todo => (
+               {filteredTodos.map(todo => (
                     <Todo 
                         key={todo.id} 
                         text={todo.text} 
