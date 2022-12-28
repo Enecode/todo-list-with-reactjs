dde4fca0f4da2a55bb26eb6912639cb95c052e09
Isah Jacob <isahjacob0@gmail.com>
2022-12-28 03:10:18 +0100

    Todo Component handling the events that are triggered by the user and passing the data to the TodoList component.

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -3,23 +3,27 @@ import React from 'react';
 const Todo = ({text, todos, todo, setTodos}) => {
     const deleteHandler = () => {
         setTodos(todos.filter((t) => t.id !== todo.id));
-    }
+    };
 
     const completeHandler = () => {
-        setTodos(todos.filter((t) => { if (t.id !== todo.id){
+        setTodos(
+            todos.map((t) => { 
+            if (t.id === todo.id){
             return{
-                ...t, completed: !t.completed
+                ...t, 
+                completed: !t.completed,
             };
         }
         return t;
-    }))
-}
+    })
+    );
+}; 
 
     
     return(
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button onClick={completeHandler} className='complete-btn' >+</button>
+            <li className={`todo-item ${todo.completed ? "completed": ""}`}>{text}</li>
+            <button onClick={completeHandler} className='completed-btn' >+</button>
             <button onClick={deleteHandler} className='trash-btn'>X</button>
        </div>
 
